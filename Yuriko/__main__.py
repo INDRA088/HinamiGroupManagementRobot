4c76f76f724c587a0cd51623f7a862ea6ade0b8a
DARKLEGEND <93474939+DARKLEGENDDARKNESSGOD@users.noreply.github.com>
2022-04-20 22:16:29 +0530

    Update __main__.py

--- Yuriko/__main__.py
+++ Yuriko/__main__.py
@@ -251,7 +251,7 @@ def start(update: Update, context: CallbackContext):
             ),
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ", url="t.me/Hinami_Support")]]
+                [[InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url=*"t.me/Hinami_Support"*)]]
             ),
         )
         
